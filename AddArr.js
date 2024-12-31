3cdb3cdb10e0ad1de698dd1647b4cb668576acfc
Anushrikatekhaye <144015639+Anushrikatekhaye@users.noreply.github.com>
2024-12-31 20:31:37 +0530

    Add files via upload

---
+++ AddArr.js
@@ -0,0 +1,27 @@
+//// addition of array
+
+//let nam = ["Ab", "Ca", "Fb","Bj", "Zz", "MN", "Ww"];
+//let nam1 = [ 1,2,3,4,5,6,];
+//let nam2 = [7,8,9,1];
+//console.log(nam.concat(nam1, nam2));
+
+////second program
+
+//let arr1 = [1,2,3,4,5,6,7,8,9,];
+////syn:array.copyWithin(target_index,start,end);
+//console.log(arr1.copyWithin(1,5,8));
+
+//// third program
+
+//let arr1 = [1, 2, 3, 4, 5, 6,"LCO", 8];
+//console.log(arr1.indexOf("LCO"));
+
+////MAP
+let asqr = [1,4,9,16];
+//syntax: array.map(expr)
+//console.log(asqr.map(Math.sqrt));
+
+//console.log(asqr.reverse());
+
+console.log(asqr.shift());
+console.log(asqr);
