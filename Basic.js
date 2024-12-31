d69e943cb05d2b9e5a44098d84a4d14cb57e74b3
Anushrikatekhaye <144015639+Anushrikatekhaye@users.noreply.github.com>
2024-12-31 20:27:28 +0530

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,34 @@
+let rating ="5";
+
+if(rating ==5) {s
+    console.log(" Excellent");
+} 
+else if (rating == 4){
+    console.log( "Rating 4");
+} else {
+    console.log(" Not Given");
+}
+console.log(" completed");
+
+
+
+
+console.log(" Redirect to homepage");
+} else if (Mobile == true && email == flase) {
+console.log("Rediect to Email verificaton");
+} else if (" Mobile == true && email == false"){
+    console.log("Rediect to Email verification");
+} else {
+    console.log(" Create your account");
+}
+
+ conditon ? expression : expression 
+
+ var loggedin = flase;
+loggedin ? console.log(" HOME PAGE") : console.log("Login first");
+if (! loggedin) {
+
+} else {
+    console.log("ome page")
+}
+
