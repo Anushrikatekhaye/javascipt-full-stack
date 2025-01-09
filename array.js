ab913becaf3da0ade85c1bff83c61baafc9721e6
Anushrikatekhaye <144015639+Anushrikatekhaye@users.noreply.github.com>
2025-01-09 10:58:37 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+ array
+ let Arr1 =["Anushri", "Ravindra", "Vanita","Devang"];
+ console.log(Arr1);
+ console.log(Arr1.length);
+ console.log(Arr1[Arr1.length-1]);
+Arr1[3] = "Anirudh";
+console.log(Arr1);
+
+
+
+
+
+
---
+++ array1.js
@@ -0,0 +1,18 @@
+//first program
+
+let Arr1 =["Anu", "ravindra","vanita","devang"];
+console.log(Arr1);
+
+// push one array
+Arr1.push("Anurag");
+console.log(Arr1);
+
+//syntax of slice :array.slice(startindex,endindex)
+let Arr2 = Arr1.slice(1 ,4)
+console.log(Arr2);
+
+
+
+
+
+
